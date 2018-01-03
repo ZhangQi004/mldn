a90a18cca719115245a20c291a0c1f1c79a3050c
ZhangQi004 <18710471124@163.com>
2018-01-03 22:49:54 +0800

    Last update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
 	public static final String comm=1;
 	public static void main(String args[]){
 		private Integer empno
-		System.out.println("www.yook.com");
+		//System.out.println("www.yook.com");
 		System.out.println("www.mldn.cn");
+		System.out.println("www.newyear.cn");
 	}
 }
