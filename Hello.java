58eaa1e9f5777b8fa5150d0b098dad6775b49fe7
ZhangQi004 <18710471124@163.com>
2018-01-03 21:36:23 +0800

    New Java file -Hello.java create

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static final String comm=1;
+	public static void main(String args[]){
+		private Integer empno
+		System.out.println("www.yook.com");
+		System.out.println("www.mldn.cn");
+		System.out.println("Hello Woor789+7ldfd*");
+		System.out.println("dsfdsfsds");
+	}
+}
