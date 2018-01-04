7dbc0419a8883f4e27419b804af4e13791d36abc
ZhangQi004 <18710471124@163.com>
2018-01-04 22:42:45 +0800

    Modified Hello.java File

--- Hello.java
+++ Hello.java
@@ -2,8 +2,6 @@ public class Hello{
 	public static final String comm=1;
 	public static void main(String args[]){
 		private Integer empno
-		//System.out.println("www.yook.com");
-		System.out.println("www.mldn.cn");
-		System.out.println("www.yook.com");
+		System.out.println("Happy New Year");
 	}
 }
